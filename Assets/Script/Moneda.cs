2c2a0f97d1dc4b00e311d7de4dca91bed873f9c1
Javiornitier <javilob97@gmail.com>
2016-11-09 18:21:58 +0100

    Prefab Moneda
    Creados los scripts fisicas, collider, fuerza random y autodestrucción.

---
+++ Assets/Script/Moneda.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moneda : MonoBehaviour {
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+		Destroy (gameObject, 3);
+		rb.AddForce (new Vector2 (Random.Range(-200, 200), 200));
+			
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
