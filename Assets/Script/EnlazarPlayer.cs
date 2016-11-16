d6c4f740156fde733de899c2472398b68ab52112
IvanGiron <ivansioncss@gmail.com>
2016-11-16 18:51:12 +0100

    Menu Pausa y mas plataformas
    Menu pausa y mas scripts de enlace.

---
+++ Assets/Script/EnlazarPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(PolygonCollider2D))]
+
+public class EnlazarPlayer : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.parent = transform;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		if(col.gameObject.tag == "Player"){
+			col.gameObject.transform.parent = null;
+		}
+	}
+}
