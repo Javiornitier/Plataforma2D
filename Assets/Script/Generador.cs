0157ba014ef94c66393928f4f7f967b5d01eca0b
Javiornitier <javilob97@gmail.com>
2016-11-23 18:41:37 +0100

    Generador de monedas aleatorio
    Creamos en el menu "Generador" un codigo para hacer que salgan 3 tipos
    de monedas.

--- Assets/Script/Generador.cs
+++ Assets/Script/Generador.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class Generador : MonoBehaviour {
-	public GameObject Moneda;
+	public GameObject[] Monedas;
 	private GameObject monedaNueva;
+	public Transform posSalida;
+	private int numeroMoneda = 0;
 
+	void Start(){
+		posSalida = transform.Find ("PosicionSalida").transform;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player" && monedaNueva == null) {
+			
+			numeroMoneda = Random.Range (0, Monedas.Length);
+			//Instantiate (Moneda, transform.position, transform.rotation); //&& "Y" || "o"
+			monedaNueva = (GameObject)Instantiate (Monedas[numeroMoneda], posSalida.position, transform.rotation);
+		}
+	}
 	void OnTriggerEnter2D(Collider2D objeto){
 		if (objeto.tag == "Player" && monedaNueva == null) {
-			//Instantiate (Moneda, transform.position, transform.rotation);
-			monedaNueva = (GameObject)Instantiate (Moneda, transform.position, transform.rotation);
+			monedaNueva = (GameObject)Instantiate (Monedas[0], transform.position, transform.rotation);
 		}
 	}
 }
-	
