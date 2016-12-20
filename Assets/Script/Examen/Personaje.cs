d76b7eb810ec508ad7aab249da4dd448e67ba484
Javiornitier <javilob97@gmail.com>
2016-12-20 13:38:38 +0100

    EstudioExamen1
    CicloAndar

---
+++ Assets/Script/Examen/Personaje.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Personaje : MonoBehaviour {
+
+private Rigidbody2D rb;
+	public float velocidad = 100f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			MovimientoDerecha ();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			MovimientoIzquierda ();
+		}
+		//if (Input.GetKeyDown (KeyCode.W)) {
+			//MovimientoArriba ();
+	}
+
+	void MovimientoDerecha (){
+			rb.velocity = new Vector2(velocidad, rb.velocity.y);
+			this.transform.localScale = new Vector3(1, 1, 1);
+		}
+	void MovimientoIzquierda (){
+			rb.velocity = new Vector2(-velocidad, rb.velocity.y);
+			this.transform.localScale = new Vector3(-1, 1, 1);
+		}
+	}
